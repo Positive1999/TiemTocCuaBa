6b9855ff365b4dc3939da9a9b8315c7c911bb720
TVHieu99 <hieutrantran102@gmail.com>
2023-11-07 10:57:39 +0700

    add

--- oclock.js
+++ oclock.js
@@ -1,12 +1,12 @@
-  // Lấy thời gian hiện tại
-  var currentTime = new Date();
-  var currentHour = currentTime.getHours();
+// Lấy thời gian hiện tại
+var currentTime = new Date();
+var currentHour = currentTime.getHours();
 
-  // Kiểm tra nếu thời gian hiện tại nằm trong khoảng mở cửa
-  if (currentHour >= 8 && currentHour < 18) {
-    document.getElementById("openStatus").classList.remove("d-none"); // Hiển thị phần tử mở cửa
-    document.getElementById("closeStatus").classList.add("d-none"); // Ẩn phần tử đóng cửa
-  } else {
-    document.getElementById("openStatus").classList.add("d-none"); // Ẩn phần tử mở cửa
-    document.getElementById("closeStatus").classList.remove("d-none"); // Hiển thị phần tử đóng cửa
-  }
+// Kiểm tra nếu thời gian hiện tại nằm trong khoảng mở cửa
+if (currentHour >= 8 && currentHour < 12 || currentHour >= 14 && currentHour < 18) {
+  document.getElementById("openStatus").classList.remove("d-none"); // Hiển thị phần tử mở cửa
+  document.getElementById("closeStatus").classList.add("d-none"); // Ẩn phần tử đóng cửa
+}else{
+  document.getElementById("openStatus").classList.add("d-none"); // Ẩn phần tử mở cửa
+  document.getElementById("closeStatus").classList.remove("d-none"); // Hiển thị phần tử đóng cửa
+}
