909687c570a80e7a094ec69232e25ee10c7d0487
TVHieu99 <hieutrantran102@gmail.com>
2023-11-09 12:43:28 +0700

    add

--- js/index.js
+++ js/index.js
@@ -1,4 +1,16 @@
+var sliderImg = document.querySelector('.slider img');
 
+changeImg2 = function(){
+    sliderImg.src = 'images/banner.jpg';
+    setTimeout(changeImg, 3000); // Thay đổi ảnh sau 3 giây
+}
+
+changeImg = function(){
+    sliderImg.src = 'images/banner2.jpg';
+    setTimeout(changeImg2, 3000); // Thay đổi ảnh sau 3 giây
+}
+
+changeImg(); // Bắt đầu thay đổi ảnh
 
 
 
