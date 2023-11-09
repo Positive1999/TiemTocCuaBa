af08704f427fc74c59595cc55c777be7d346fe88
TVHieu99 <hieutrantran102@gmail.com>
2023-11-09 19:32:00 +0700

    add

--- js/index.js
+++ js/index.js
@@ -10,41 +10,44 @@ setInterval(function() {
 
 
 var connectedEmail = document.querySelector('.connected #email');
-var NoneBlockResult= document.querySelector('.Notification');
+var NoneBlockResult = document.querySelector('.Notification');
 var ConnectedResult = document.querySelector('.Notification__result');
-const btnConnected = document.querySelector('.btn-connected');
+const btnSubmitConnected = document.querySelector('.btn-connected');
 
-btnConnected.addEventListener('click', ()=>{
-  noneNotification = ()=>{
-    NoneResult.style.display = 'none';
-  }
-  // Kiểm tra định dạng email
-  const emailRegex = /^[^\s@]+@[^\s@]+.[^\s@]+$/;
-  if(connectedEmail.value === ''){
-  // Nếu email trống
-  NoneBlockResult.style.display= 'block';
-  ConnectedResult.style.backgroundColor = 'red';
-  ConnectedResult.style.color = '#fff';
-  ConnectedResult.innerHTML = 'Vui lòng điền email';
-  setTimeout(NoneBlockResult,2000);
+btnSubmitConnected.addEventListener('click', async () => {
+  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  
+  if (connectedEmail.value === '') {
+    NoneBlockResult.style.display = 'block';
+    ConnectedResult.style.backgroundColor = 'red';
+    ConnectedResult.style.color = '#fff';
+    ConnectedResult.innerHTML = 'Vui lòng điền email';
+    
+    await delay(2000);
+    NoneBlockResult.style.display = 'none';
   } else if (!emailRegex.test(connectedEmail.value)) {
-  // Nếu email không đúng định dạng
-  NoneBlockResult.style.display= 'block';
-  ConnectedResult.style.backgroundColor = 'yellow';
-  ConnectedResult.style.color = '#000000';
-  ConnectedResult.innerHTML = 'Vui lòng điền đúng định dạng email';
-  setTimeout(NoneBlockResult,2000);
+    NoneBlockResult.style.display = 'block';
+    ConnectedResult.style.backgroundColor = 'yellow';
+    ConnectedResult.style.color = '#000000';
+    ConnectedResult.innerHTML = 'Vui lòng điền đúng định dạng email';
+    
+    await delay(2000);
+    NoneBlockResult.style.display = 'none';
   } else {
-  // Nếu email đúng định dạng
-  // Tiến hành xử lý logic
-  NoneBlockResult.style.display= 'block';
-  ConnectedResult.style.backgroundColor = '#00ff00';
-  ConnectedResult.style.color = '#000000';
-  ConnectedResult.innerHTML = 'Gửi thành công !';
-  connectedEmail.value = '';
-  setInterval(NoneBlockResult,2000);
+    NoneBlockResult.style.display = 'block';
+    ConnectedResult.style.backgroundColor = '#00ff00';
+    ConnectedResult.style.color = '#000000';
+    ConnectedResult.innerHTML = 'Gửi thành công !';
+    connectedEmail.value = '';
+    
+    await delay(2000);
+    NoneBlockResult.style.display = 'none';
   }
-  })
+});
+
+function delay(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
 
 
 
